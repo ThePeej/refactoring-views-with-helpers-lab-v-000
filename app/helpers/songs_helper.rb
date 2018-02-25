655512d3d14adbdce9d92c0eb2f4611b3b2ef5bc
PJ Frias <peejfrias@gmail.com>
2018-02-25 20:27:56 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,9 @@
 module SongsHelper
 
   def display_artist(song)
-    artist_path(song.artist) if !!song.artist
+    if !!song.artist
+      artist_path(song.artist)
+    else
+      link_to edit_song_path(song), "Add Artist"
 
 end
